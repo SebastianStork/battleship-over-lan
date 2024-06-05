2c73ad7a975e6ffbce8d9ad0d2a14b151388f526
NebelToast <juliussteude@gmail.com>
2024-06-05 17:56:06 +0200

    Implemented placeShipsRandomly

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Board {
     private Position[][] positions;
     private Ship[] ships;
@@ -12,11 +14,82 @@ public class Board {
     public void placeShips(Ship[] ships){
         this.ships = ships;
     }
-    public void placeShipsRandomly(){
 
+    public void placeShipsRandomly() {
+        Random random = new Random();
+        int[] shipSizes = {5, 4, 4, 3, 3, 3, 2, 2, 2, 2};
+        ships = new Ship[10];
+        int index = 0;
+
+        for (int size : shipSizes) {
+            boolean placed = false;
+
+            while (!placed) {
+                boolean isLegal = true;
+                int row = random.nextInt(10);
+                int col = random.nextInt(10);
+                boolean horizontal = random.nextBoolean();
+
+                if (horizontal) {
+                    if (col + size > 10 || row + size >10) {
+                        isLegal = false;
+                    } else {
+                        for (int i = 0; i < size; i++) {
+                            if (positions[row][col + i].isOccupied()) {
+                                isLegal = false;
+                                break;
+                            }
+                        }
+                        for (int i = 0; i < size && isLegal; i++) {
+                            for (int dr = -1; dr <= 1; dr++) {
+                                for (int dc = -1; dc <= 1; dc++) {
+                                    int newRow = row + dr;
+                                    int newCol = col + i + dc;
+                                    if (newRow >= 0 && newRow < 10 && newCol >= 0 && newCol < 10) {
+                                        if (positions[newRow][newCol].isOccupied()) {
+                                            isLegal = false;
+                                            break;
+                                        }
+                                    }
+                                }
+                                if (!isLegal) break;
+                            }
+                        }
+                    }
+                } else {
+                    if (row + size > 10) {
+                        isLegal = false;
+                    } else {
+
+                        for (int i = 0; i < size && isLegal; i++) {
+                            for (int dr = -1; dr <= 1; dr++) {
+                                for (int dc = -1; dc <= 1; dc++) {
+                                    int newRow = row + i + dr;
+                                    int newCol = col + dc;
+                                    if (newRow >= 0 && newRow < 10 && newCol >= 0 && newCol < 10) {
+                                        if (positions[newRow][newCol].isOccupied()) {
+                                            isLegal = false;
+                                            break;
+                                        }
+                                    }
+                                }
+                                if (!isLegal) break;
+                            }
+                        }
+                    }
+                }
+
+                if (isLegal) {
+                    ships[index] = new Ship(size, horizontal ? 0 : 1, new Position());
+
+                    placed = true;
+                    index++;
+                }
+            }
+        }
     }
     public void setShotAt(int col, int row) {
-        Position pos = positions[col][row];
+        Position pos = Positions[col][row];
         pos.setToHit();
         if (pos.isOccupied()) {
             for (Ship ship : ships) {
