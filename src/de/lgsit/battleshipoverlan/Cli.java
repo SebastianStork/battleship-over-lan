0c9673db31f2b4a7c0a05e35ce1648f2a494f145
SebastianStork <sebastian.stork@pm.me>
2024-06-10 18:30:42 +0200

    Init cli class

---
+++ src/de/lgsit/battleshipoverlan/Cli.java
@@ -0,0 +1,11 @@
+package de.lgsit.battleshipoverlan;
+
+import java.util.Scanner;
+
+public class Cli {
+    private final Scanner stdin;
+
+    public Cli() {
+        stdin = new Scanner(System.in);
+    }
+}
