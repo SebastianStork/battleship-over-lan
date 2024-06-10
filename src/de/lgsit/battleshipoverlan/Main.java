ce93bc10ae8308d3965886eb0c6317b8d118c4f9
SebastianStork <sebastian.stork@pm.me>
2024-06-10 18:45:39 +0200

    Implement the mode selector

--- src/de/lgsit/battleshipoverlan/Main.java
+++ src/de/lgsit/battleshipoverlan/Main.java
@@ -1,7 +1,39 @@
 package de.lgsit.battleshipoverlan;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        System.out.println("Welcome to Battleship-Over-LAN!");
+        System.out.println();
+
+        System.out.println("Would you like to");
+        System.out.println("(h) host a new game?");
+        System.out.println("(j) join an existing game?");
+        System.out.println("(q) quit the game?");
+        System.out.println();
+
+        System.out.print("Choice [hjq]: ");
+        Scanner stdin = new Scanner(System.in);
+        String line = stdin.nextLine();
+
+        while (!Arrays.asList("h", "j", "q").contains(line)) {
+            System.out.printf("'%s' is invalid!\r\n", line);
+            System.out.print("Choice [hjq]: ");
+            line = stdin.nextLine();
+        }
+
+        switch (line) {
+            case "h":
+                new Server();
+                break;
+            case "j":
+                new Client();
+                break;
+            case "q":
+                System.exit(0);
+        }
     }
 }
