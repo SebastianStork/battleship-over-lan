153cdcf33d529cd00d767997ad035c8fe96178a2
SebastianStork <sebastian.stork@pm.me>
2024-06-21 14:14:18 +0200

    Package program with nix

--- flake.nix
+++ flake.nix
@@ -2,15 +2,39 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 
   outputs =
-    { nixpkgs, ... }:
+    { self, nixpkgs, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
-      devShells.${system}.default = pkgs.mkShell { 
+      devShells.${system}.default = pkgs.mkShell {
         packages = [ pkgs.jdk ];
         JAVA_HOME = pkgs.jdk.home;
       };
+
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        name = "battleship-over-lan";
+        src = self;
+
+        nativeBuildInputs = [
+          pkgs.jdk
+          pkgs.makeWrapper
+        ];
+
+        buildPhase = ''
+          cd src
+          javac de/lgsit/battleshipoverlan/*.java
+          jar -cf $out/share/java/battleship-over-lan.jar de/lgsit/battleshipoverlan/*.class
+        '';
+
+        installPhase = ''
+          mkdir -p $out/bin
+          makeWrapper ${pkgs.jre}/bin/java $out/bin/battleship-over-lan \
+            --add-flags "-cp $out/share/java/battleship-over-lan.jar de.lgsit.battleshipoverlan.Main"
+        '';
+      };
+
+      formatter.${system} = pkgs.nixfmt-rfc-style;
     };
 }
